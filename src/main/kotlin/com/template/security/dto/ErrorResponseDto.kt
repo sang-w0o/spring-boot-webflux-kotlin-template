987f7ffc198886978fd79e09f8fdcaa1ce78f4a0
sang-w0o <robbyra@gmail.com>
2021-10-06 20:34:03 +0900

    feat: Create ErrorResponseDto

--- src/main/kotlin/com/template/security/dto/ErrorResponseDto.kt
+++ src/main/kotlin/com/template/security/dto/ErrorResponseDto.kt
@@ -1,4 +1,18 @@
 package com.template.security.dto
 
-class ErrorResponseDto {
-}
+import com.fasterxml.jackson.annotation.JsonFormat
+import java.time.LocalDateTime
+
+data class ErrorResponseDto(
+    @field:JsonFormat(
+        shape = JsonFormat.Shape.STRING,
+        pattern = "yyyy-MM-dd HH:mm:ss",
+        locale = "Asia/Seoul"
+    )
+    val timestamp: LocalDateTime,
+    val status: Int,
+    val error: String,
+    val message: String,
+    val path: String,
+    val remote: String?
+)
