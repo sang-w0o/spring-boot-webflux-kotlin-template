a5348242416c1cc31221c1a62aa04d20614ccb16
sang-w0o <robbyra@gmail.com>
2021-10-08 10:56:42 +0900

    chore: Expose Actuator APIs

--- src/main/kotlin/com/template/security/config/SecurityConfig.kt
+++ src/main/kotlin/com/template/security/config/SecurityConfig.kt
@@ -1,6 +1,7 @@
 package com.template.security.config
 
 import org.springframework.context.annotation.Bean
+import org.springframework.http.HttpMethod
 import org.springframework.security.authentication.ReactiveAuthenticationManager
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder
@@ -10,6 +11,8 @@ import org.springframework.security.web.server.SecurityWebFilterChain
 import org.springframework.security.web.server.authentication.AuthenticationWebFilter
 import org.springframework.security.web.server.authentication.ServerAuthenticationConverter
 import org.springframework.security.web.server.authorization.ServerAccessDeniedHandler
+import org.springframework.security.web.server.util.matcher.NegatedServerWebExchangeMatcher
+import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers
 
 @EnableWebFluxSecurity
 class SecurityConfig(
@@ -22,12 +25,17 @@ class SecurityConfig(
         val jwtAuthenticationWebFilter = AuthenticationWebFilter(jwtAuthenticationManager)
         jwtAuthenticationWebFilter.setServerAuthenticationConverter(jwtAuthenticationConverter)
         return http {
+            securityMatcher(
+                NegatedServerWebExchangeMatcher(ServerWebExchangeMatchers.pathMatchers(HttpMethod.GET, "/actuator/**"))
+            )
+            authorizeExchange { authorize(anyExchange, authenticated) }
             httpBasic { disable() }
             formLogin { disable() }
             csrf { disable() }
             logout { disable() }
             authorizeExchange {
                 authorize("/v1/**", authenticated)
+                authorize("/actuator/**", permitAll)
             }
             addFilterAt(jwtAuthenticationWebFilter, SecurityWebFiltersOrder.AUTHENTICATION)
             exceptionHandling {
