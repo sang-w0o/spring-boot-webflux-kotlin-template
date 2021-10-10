d6dfc2a74bc5201e1b7f3d2680e0906c0ff5045e
sang-w0o <robbyra@gmail.com>
2021-10-10 17:36:54 +0900

    fix: Use LocalDateTime.now(clock) to avoid FileInputStream.readBytes() blocking call

--- src/main/kotlin/com/template/Application.kt
+++ src/main/kotlin/com/template/Application.kt
@@ -4,25 +4,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-import reactor.blockhound.BlockHound
-import java.io.FileInputStream
+import java.time.Clock
 
 @SpringBootApplication
 class Application {
     @Bean
-    fun passwordEncoder(): BCryptPasswordEncoder {
-        return BCryptPasswordEncoder(10)
-    }
+    fun passwordEncoder(): BCryptPasswordEncoder = BCryptPasswordEncoder(10)
+
+    @Bean
+    fun clock(): Clock = Clock.systemDefaultZone()
 }
 
 fun main(args: Array<String>) {
-//    BlockHound.builder()
-//        .allowBlockingCallsInside(FileInputStream::class.java.canonicalName, "readBytes")
-//        .install()
-
-//    BlockHound.install()
-    BlockHound.builder()
-        .allowBlockingCallsInside("java.io.FileInputStream", "readBytes")
-//        .install()
     runApplication<Application>(*args)
 }
