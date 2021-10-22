7af2080910a48bda8bf669fa736cd59bad1a054d
sang-w0o <robbyra@gmail.com>
2021-10-23 01:47:14 +0900

    chore: Integrate exception handler for DecodingException and WebExchangeBindException

--- src/main/kotlin/com/template/config/handler/RequestBodyValidatingExceptionHandler.kt
+++ src/main/kotlin/com/template/config/handler/RequestBodyValidatingExceptionHandler.kt
@@ -2,13 +2,19 @@ package com.template.config.handler
 
 import com.template.config.exception.InvalidMemberFieldException
 import org.springframework.context.support.DefaultMessageSourceResolvable
-import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.core.codec.DecodingException
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
 import org.springframework.web.bind.support.WebExchangeBindException
 import java.util.stream.Collectors
 
-@ControllerAdvice
-class ValidationExceptionHandler {
+@RestControllerAdvice
+class RequestBodyValidatingExceptionHandler {
+
+    @ExceptionHandler(DecodingException::class)
+    fun handleDecodingException(exception: DecodingException) {
+        throw InvalidMemberFieldException("Wrong request body received.")
+    }
 
     @ExceptionHandler(WebExchangeBindException::class)
     fun handleWebExchangeBindException(exception: WebExchangeBindException) {
