cf96a8494ada7211077746601750876fcec20ae0
sang-w0o <robbyra@gmail.com>
2022-10-18 00:43:02 +0900

    refactor: Dto에 불필요한 annotation 제거

--- src/main/kotlin/com/template/user/dto/AccessTokenUpdateRequestDto.kt
+++ src/main/kotlin/com/template/user/dto/AccessTokenUpdateRequestDto.kt
@@ -1,8 +1,5 @@
 package com.template.user.dto
 
-import javax.validation.constraints.NotBlank
-
 data class AccessTokenUpdateRequestDto(
-    @field:NotBlank(message = "refreshToken is required.")
     val refreshToken: String = ""
 )
