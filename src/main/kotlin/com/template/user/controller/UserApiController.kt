3f59593ba071470300ca616e79e023affced7c80
sang-w0o <robbyra@gmail.com>
2022-10-18 00:42:38 +0900

    refactor: controller에서 service 호출 시 dto로 변경해서 전달

--- src/main/kotlin/com/template/user/controller/UserApiController.kt
+++ src/main/kotlin/com/template/user/controller/UserApiController.kt
@@ -1,11 +1,18 @@
 package com.template.user.controller
 
 import com.template.config.annotation.LoggedInUser
+import com.template.user.controller.request.AccessTokenUpdateRequest
+import com.template.user.controller.request.UserCreateRequest
+import com.template.user.controller.request.UserLoginRequest
+import com.template.user.controller.response.AccessTokenUpdateResponse
+import com.template.user.controller.response.UserInfoResponse
+import com.template.user.controller.response.UserLoginResponse
 import com.template.user.domain.User
-import com.template.user.dto.*
 import com.template.user.service.UserService
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import reactor.core.publisher.Mono
+import java.net.URI
 import javax.validation.Valid
 
 @RestController
@@ -15,14 +22,26 @@ class UserApiController(
 ) {
 
     @PostMapping("/signup")
-    fun createUser(@RequestBody @Valid requestDto: Mono<UserCreateRequestDto>) = userService.create(requestDto)
+    fun createUser(@RequestBody @Valid request: Mono<UserCreateRequest>): Mono<ResponseEntity<UserInfoResponse>> {
+        return userService.create(request.map { it.toDto() })
+            .map { ResponseEntity.created(URI.create("/v1/user")).body(UserInfoResponse.from(it.body!!)) }
+    }
 
     @PostMapping("/login")
-    fun login(@RequestBody @Valid requestDto: Mono<UserLoginRequestDto>) = userService.login(requestDto)
+    fun login(@RequestBody @Valid request: Mono<UserLoginRequest>): Mono<ResponseEntity<UserLoginResponse>> {
+        return userService.login(request.map { it.toDto() })
+            .map { ResponseEntity.ok().body(UserLoginResponse.from(it.body!!)) }
+    }
 
     @GetMapping("/info")
-    fun getUserInfo(@LoggedInUser user: User) = userService.getInfo(Mono.just(user))
+    fun getUserInfo(@LoggedInUser user: User): Mono<ResponseEntity<UserInfoResponse>> {
+        return userService.getInfo(Mono.just(user))
+            .map { ResponseEntity.ok().body(UserInfoResponse.from(it.body!!)) }
+    }
 
     @PostMapping("/update-token")
-    fun updateAccessToken(@RequestBody @Valid dto: Mono<AccessTokenUpdateRequestDto>) = userService.updateAccessToken(dto)
+    fun updateAccessToken(@RequestBody @Valid request: Mono<AccessTokenUpdateRequest>): Mono<ResponseEntity<AccessTokenUpdateResponse>> {
+        return userService.updateAccessToken(request.map { it.refreshToken })
+            .map { ResponseEntity.ok().body(AccessTokenUpdateResponse.from(it.body!!)) }
+    }
 }
